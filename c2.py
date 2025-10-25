4a8c497d49ed3c60398044f2c9813431ef014e1c
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-10-25 15:00:29 +0400

    Completed 2 - NumPy Array Indexing https://www.w3schools.com/python/numpy/numpy_array_indexing.asp Learnt  how to access numbers from 1-D, 2-D, 3-D arrays Learnt how to do negative indexing

--- c2.py
+++ c2.py
@@ -1,2 +1,27 @@
 import numpy as np
-#teswt
+
+# accessing 1-D arrays
+
+array = np.array([1, 2, 3, 4])
+
+print(array[0])
+
+print(array[1] + array[3])
+
+# accessing 2-D arrays
+
+two_array = np.array([[1, 2, 3, 4, 5],
+                     [6, 7, 8, 9, 10]])
+
+print(two_array[1, 4])
+
+# accessing 3-D arrays
+
+three_array = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], 
+               [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+
+print(three_array[1, 0, 2])
+
+# negative indexing
+
+print(three_array[-1, -1, -2])
