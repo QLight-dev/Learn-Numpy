f1e08234f3990b35c172586d2eed15a236abb426
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-10-25 16:37:14 +0400

    Completed 3 - NumPy Array Slicing https://www.w3schools.com/python/numpy/numpy_array_slicing.asp Learnt how to slice arrays in formats: [start:end] [start:] [end:] [start:end:step] [::step] Learnt how to Negative slice Learnt how to slice 2D arrays

--- c3.py
+++ c3.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-# [start:end:step]
-
 array = np.array([1, 2, 3, 4, 5])
 
-print(array[4:5])
+print(f"[start:end]: {array[2:5]}")
+print(f"[start:]: {array[3:]}")
+print(f"[:end]: {array[:4]}")
+
+print(f"Negative slicing: {array[-5:-1]}")
+
+print(f"[start:end:step]: {array[2:5:2]}")
+print(f"[::step]: {array[::2]}")
+
+two_array = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(f"Slicing 2-D arrays: {two_array[1, 2:5]}")
