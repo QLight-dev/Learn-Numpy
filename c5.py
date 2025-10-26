391d6d6178bedceeaec7f39a17fad2c78a38a302
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-10-26 14:23:42 +0400

    Completed 5 - NumPy Array Copy vs View https://www.w3schools.com/python/numpy/numpy_copy_vs_view.asp Learnt how to copy arrays onto another array Learnt how to view arrays onto another array Learnt how to check the base of a copied and Viewed array

--- c5.py
+++ c5.py
@@ -1,1 +1,21 @@
-import numpy as np
+import numpy as np 
+
+# Copy
+array = np.array([1, 2, 3, 4, 5])
+copied_array = array.copy()
+array[0] = 67
+
+print(f"Original Array: {array}")
+print(f"Copied Array (Edited): {copied_array}")
+
+# View
+array = np.array([1, 2, 3, 4, 5])
+viewed_array = array.view()
+array[0] = 67
+
+print(f"Original Array: {array}")
+print(f"Viewed Array: {viewed_array}")
+
+# Check if Array Owns its Data
+print(f"Copied Array Base: {copied_array.base}")
+print(f"Viewed Array Base: {viewed_array.base}")
