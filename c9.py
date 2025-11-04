4857740ff2102216eed01d7b77a89895815e5883
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-04 06:02:24 +0000

    9 - NumPy Array Join https://www.w3schools.com/python/numpy/numpy_array_join.asp Learnt how to join two arrays Learnt how to join 2-D arrays along rows Learnt how to Join Arrays using stack functions and more

--- c9.py
+++ c9.py
@@ -1,1 +1,37 @@
 import numpy as np
+
+# Joining
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(np.concatenate((array1, array2)))
+
+# Joining using axis
+array1 = np.array([[1, 2], [3, 4]])
+array2 = np.array([[5, 6], [7, 8]])
+
+print(np.concatenate((array1, array2), axis=1))
+
+# Stacking
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(np.stack((array1, array2), axis=1))
+
+# Stacking along Rows
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(np.hstack((array1, array2)))
+
+# Stacking along Columns
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(np.vstack((array1, array2)))
+
+# Stacking along Height (depth)
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(np.dstack((array1, array2)))
