92c1e55de334b2c8af67a943d6dbcaed0648de75
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-05 04:24:47 -0800

    Completed 10 - NumPy Array Slicing
    https://www.w3schools.com/python/numpy/numpy_array_split.asp Learnt how to split Arrays Learnt hwo to split 2-D, Learnt how to use hsplit()

--- c10.py
+++ c10.py
@@ -11,3 +11,5 @@ print(f"Splitting 2-D : {np.array_split(array, 4)}")
 # Using hsplit()
 array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
 print(f"Using hsplit(): {np.hsplit(array, 3)}")
+
+# ...
