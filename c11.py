42872b4efd993d26166cc01abf146d2fbc9f61c3
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-07 19:02:08 +0400

    Completed 11 - NumPy Searching Arrays https://www.w3schools.com/python/numpy/numpy_array_search.asp Learned how to Search Arrays Learned how to Search Sorted Learned how to Search From the Right Side Learned how to Search for multiple values

--- c11.py
+++ c11.py
@@ -7,3 +7,12 @@ print(f"Searching Arrays: {np.where(array == 3)}")
 # Search Sorted
 array = np.array([1, 6, 1, 3, 4, 5])
 print(f"Search Sorted: {np.searchsorted(array, 6)}")
+
+# Search from the Right Side
+array = np.array([2, 3, 4, 5])
+print(f"Search from the Right Side: {np.searchsorted(array, 3, side="right")}")
+
+# Search for multiple values
+array = np.array([2, 4, 6, 8])
+print(f"Search for multiple values: {np.searchsorted(array, [1, 3, 5, 7])}")
+
