0d2044957f76cd57ae3f03574fddc68975317950
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-07 08:47:27 +0000

    Nearly done Chapter 11

--- c11.py
+++ c11.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+# Searching Arrays
+array = np.array([1, 3, 2, 3, 2, 3, 4, 5, 3, 6])
+print(f"Searching Arrays: {np.where(array == 3)}")
+
+# Search Sorted
+array = np.array([1, 6, 1, 3, 4, 5])
+print(f"Search Sorted: {np.searchsorted(array, 6)}")
