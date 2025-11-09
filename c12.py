aba0340707886db4a30442cfb480837cbfd6fa0b
Muhammed Uzair <nayazmuhammed094@gmail.com>
2025-11-09 18:03:50 +0400

    Completed 12 - NumPy Sorting Arrays https://www.w3schools.com/python/numpy/numpy_array_sort.asp Learnt how sort a 1-D int Array Learnt how to sort a 1-D string Array Learnt how to sort a 2-D string Array

--- c12.py
+++ c12.py
@@ -2,4 +2,12 @@ import numpy as np
 
 # Sorting Arrays
 array = np.array([6, 2, 4, 8, 7, 9])
-print(np.sort(array))
+print(f"Sorting Arrays: {np.sort(array)}")
+
+# Sorting a String Array
+array = np.array(["Chocolate", "Ice Cream", "Yo-yo", "Groceries"])
+print(f"Sorting a String Array: {np.sort(array)}")
+
+# Sorting a 2-D Array
+array = np.array([[6, 2, 9], [5, 8, 3]])
+print(f"Sorting a 2-D Array: {np.sort(array)}")
